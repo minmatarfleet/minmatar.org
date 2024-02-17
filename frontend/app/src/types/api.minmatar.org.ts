164c97ce86ce2ef8b250eefe5827d2de7869f016
Beautiful Mim <beautifulmim.eve@gmail.com>
2024-02-17 01:06:10 -0500

    Added available groups listing page

--- frontend/app/src/types/api.minmatar.org.ts
+++ frontend/app/src/types/api.minmatar.org.ts
@@ -2,3 +2,22 @@ export interface Character {
     character_id:   number;
     character_name: string;
 }
+
+export type GroupStatus = 'available' | 'requested' | 'confirmed'
+
+export interface Group {
+    id:             number;
+    name:           string;
+    description:    string;
+    image_url:      string;
+    status:         GroupStatus;
+}
+
+export interface GroupRequest {
+    id:             number;
+    user:           number;
+    group:          number;
+    approved:       boolean;
+    approved_by:    number;
+    approved_at:    string;
+}
