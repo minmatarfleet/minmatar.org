350b697a8035d05cc2075b845cb0b0c0084387eb
Beautiful Mim <beautifulmim.eve@gmail.com>
2024-02-21 00:21:21 -0500

    Added new lib to parse ever html and helper function

--- frontend/app/src/helpers/eve.ts
+++ frontend/app/src/helpers/eve.ts
@@ -1,4 +1,5 @@
 import type { Locales, CharacterRaces } from '@dtypes/layout_components'
+import * as cheerio from 'cheerio';
 
 export const get_zkillboard_character_link = (character_id):string => {
     return `https://zkillboard.com/character/${character_id}/`
@@ -24,4 +25,31 @@ export const get_race_cover_image = (race:CharacterRaces):string => {
     }
 
     return covers_by_race[race] ?? covers_by_race.caldari
+}
+
+export const parse_eve_html = (html:string):string => {
+    const $ = cheerio.load(html)
+    const prop_equivalent = {
+        size: 'font-size',
+        face: 'font-family',
+    }
+
+    $('font').each((i, el) => {
+        const style = {}
+        for (let prop in el.attribs) {
+            let val = el.attribs[prop]
+            if (prop == 'color')
+                val = `#${val.substring(3)}`
+
+            if (prop == 'size')
+                val = `${val}px`
+
+            style[prop_equivalent[prop] ?? prop] = val
+        }
+
+        $(el).css(style)
+        el.tagName = 'span'
+    })
+
+    return $.html()
 }
