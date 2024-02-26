f16d47de90b814687a839774d0c88c3ec61784f4
Beautiful Mim <beautifulmim.eve@gmail.com>
2024-02-25 23:51:04 -0500

    Added component with sticky header

--- frontend/app/src/i18n/ui.ts
+++ frontend/app/src/i18n/ui.ts
@@ -469,5 +469,6 @@ export const ui = {
         'icon': 'Icon',
         'list_view_mode': 'View mode (List)',
         'icons_view_mode': 'View mode (Icons)',
+        'block_missing_header_slot': '⚠️&nbsp;<b>Header slot missing:</b>&nbsp;Add&nbsp;`slot="header"`&nbsp;attribute on the element meant to be on the sticky header.',
     },
 } as const;
