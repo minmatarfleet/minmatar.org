e6205614abfb2cd54a374590fdc5229b8e97385c
Marcelo Paez Sequeira <paezao@gmail.com>
2024-03-20 10:57:39 -0300

    PR comment related changes

--- backend/mumble/apps.py
+++ backend/mumble/apps.py
@@ -1,16 +1,8 @@
 from django.apps import AppConfig
-from django.db.models.signals import post_save
-
-def create_mumble_access(sender, instance, created, **kwargs):
-    if created:
-        from .models import MumbleAccess
-        MumbleAccess.objects.create_mumble_access(instance)
 
 class MumbleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mumble'
 
     def ready(self):
-        from django.contrib.auth.models import User
-
-        post_save.connect(create_mumble_access, sender=User, dispatch_uid="create_mumble_access")
+        import groups.signals  # pylint: disable=unused-import, import-outside-toplevel
