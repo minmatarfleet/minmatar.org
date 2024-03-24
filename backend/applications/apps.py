ba0638048580890a02775f82fe611c8153013d0e
bearthatcares <bearthatcares.eve@gmail.com>
2024-03-24 00:03:00 -0400

    enable application signals

--- backend/applications/apps.py
+++ backend/applications/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ApplicationsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "applications"
+
+    def ready(self):
+        import applications.signals  # pylint: disable=unused-import, import-outside-toplevel
