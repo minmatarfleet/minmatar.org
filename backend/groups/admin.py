c44d1ccd0be961853590682ce06ad8210c486f42
bearthatcares <bearthatcares.eve@gmail.com>
2024-04-12 10:36:48 -0400

    add affiliations to admins ite

--- backend/groups/admin.py
+++ backend/groups/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Sig, Team
+from .models import Sig, Team, AffiliationType, UserAffiliation
 
 # Register your models here.
+admin.site.register(AffiliationType)
+admin.site.register(UserAffiliation)
 admin.site.register(Sig)
 admin.site.register(Team)
