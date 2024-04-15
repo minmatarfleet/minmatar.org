13ab85788986c7e520f11a3df8531fb05db5057a
bearthatcares <149488155+bearthatcares@users.noreply.github.com>
2024-04-15 14:53:23 -0400

    Add structures endpoints / structures processing (#234)
    * Add structures endpoints / structures processing
    
    * lint

--- backend/eveonline/helpers/assets.py
+++ backend/eveonline/helpers/assets.py
@@ -4,10 +4,10 @@ from typing import List, Optional
 
 import pydantic
 from esi.clients import EsiClientProvider
-from esi.models import Token
 from eveuniverse.models import EveGroup, EveStation, EveType
 
 from eveonline.models import EveCharacter, EveCharacterAsset
+from structures.models import EveStructure
 
 logger = logging.getLogger(__name__)
 esi = EsiClientProvider()
@@ -33,10 +33,6 @@ def create_character_assets(character: EveCharacter):
     """Create assets for a character"""
     logger.info("Creating assets for character %s", character.character_id)
     EveCharacterAsset.objects.filter(character=character).delete()
-    required_scopes = [
-        "esi-assets.read_assets.v1",
-        "esi-universe.read_structures.v1",
-    ]
     logger.info("Loading assets for character %s", character.character_id)
     assets: List[EveAssetResponse] = json.loads(character.assets_json)
     for asset in assets:
@@ -48,9 +44,6 @@ def create_character_assets(character: EveCharacter):
             wait_for_children=True,
         )
         if eve_type.eve_group is None:
-            logger.info(
-                "Skipping asset %s due to no market group", eve_type.name
-            )
             continue
         eve_group, _ = EveGroup.objects.get_or_create_esi(
             id=eve_type.eve_group.id,
@@ -59,9 +52,6 @@ def create_character_assets(character: EveCharacter):
         )
         eve_category = eve_group.eve_category
         if eve_category.name != "Ship":
-            logger.info(
-                "Skipping asset %s due to not being a ship", eve_type.name
-            )
             continue
         logger.info("Found asset %s", eve_type.name)
         location = None
@@ -69,24 +59,11 @@ def create_character_assets(character: EveCharacter):
             location, _ = EveStation.objects.get_or_create_esi(
                 id=asset.location_id
             )
-        elif asset.location_type == "other":
-            # fetch structure
-            token = Token.objects.filter(
-                character_id=character.character_id,
-                scopes__name__in=required_scopes,
-            ).first()
-            if token is None:
-                logger.error(
-                    "No token found for character %s",
-                    character.character_id,
-                )
+        elif asset.location_type == "item":
+            if EveStructure.objects.filter(id=asset.location_id).exists():
+                location = EveStructure.objects.get(id=asset.location_id)
+            else:
                 continue
-            location: EveStructureResponse = (
-                esi.client.Universe.get_universe_structures_structure_id(
-                    structure_id=asset.location_id,
-                    token=token.valid_access_token(),
-                ).results()
-            )
         else:
             continue
 
---
+++ backend/structures/tasks.py
@@ -0,0 +1,100 @@
+import logging
+
+from esi.clients import EsiClientProvider
+from esi.models import Token
+from eveuniverse.models import EveSolarSystem, EveType
+
+from app.celery import app
+from eveonline.models import EveCorporation
+
+from .models import EveStructure
+
+esi = EsiClientProvider()
+logger = logging.getLogger(__name__)
+
+
+@app.task
+def update_structures():
+    for corporation in EveCorporation.objects.all():
+        logger.info("Checking corporation %s for structures", corporation)
+        if corporation.ceo and corporation.ceo.token:
+            required_scopes = ["esi-corporations.read_structures.v1"]
+            if Token.objects.filter(
+                character_id=corporation.ceo.character_id,
+                scopes__name__in=required_scopes,
+            ).exists():
+                token = Token.objects.filter(
+                    character_id=corporation.ceo.character_id,
+                    scopes__name__in=required_scopes,
+                ).first()
+                logger.info(
+                    "Fetching structures for corporation %s", corporation
+                )
+                response = esi.client.Corporation.get_corporations_corporation_id_structures(
+                    corporation_id=corporation.corporation_id,
+                    token=token.valid_access_token(),
+                ).results()
+
+                known_structure_ids = []
+                for structure in response:
+                    logger.info("Processing structure %s", structure)
+                    system, _ = EveSolarSystem.objects.get_or_create_esi(
+                        id=structure["system_id"]
+                    )
+                    structure_type, _ = EveType.objects.get_or_create_esi(
+                        id=structure["type_id"]
+                    )
+                    corporation = EveCorporation.objects.get(
+                        corporation_id=structure["corporation_id"]
+                    )
+                    if EveStructure.objects.filter(
+                        id=structure["structure_id"]
+                    ).exists():
+                        logger.info(
+                            "Updating existing structure %s", structure["name"]
+                        )
+                        structure = EveStructure.objects.get(
+                            id=structure["structure_id"]
+                        )
+                        structure.state = structure["state"]
+                        structure.state_timer_start = structure[
+                            "state_timer_start"
+                        ]
+                        structure.state_timer_end = structure[
+                            "state_timer_end"
+                        ]
+                        structure.fuel_expires = structure["fuel_expires"]
+                        structure.save()
+                    else:
+                        logger.info(
+                            "Creating new structure %s", structure["name"]
+                        )
+                        structure = EveStructure.objects.create(
+                            id=structure["structure_id"],
+                            system_id=structure["system_id"],
+                            system_name=system.name,
+                            type_id=structure["type_id"],
+                            type_name=structure_type.name,
+                            name=structure["name"],
+                            reinforce_hour=structure["reinforce_hour"],
+                            state=structure["state"],
+                            state_timer_start=structure["state_timer_start"],
+                            state_timer_end=structure["state_timer_end"],
+                            corporation=corporation,
+                        )
+
+                    known_structure_ids.append(structure.id)
+
+                # delete structures that are no longer in the response
+                structures = EveStructure.objects.filter(
+                    corporation=corporation
+                )
+                for structure in structures:
+                    if structure.id not in known_structure_ids:
+                        logger.info("Deleting structure %s", structure.name)
+                        structure.delete()
+        else:
+            logger.info(
+                "Corporation %s does not have CEO token",
+                corporation,
+            )
