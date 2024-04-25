f3dfb0727d570a69f2716ba558d74b9c03d4d911
Beautiful Mim <beautifulmim.eve@gmail.com>
2024-04-25 11:09:31 -0400

    Updated countdown component

--- frontend/app/public/js/countdown.js
+++ frontend/app/public/js/countdown.js
@@ -1,15 +1,15 @@
 const countdown = (element, date, expired_text) => {
     // Set the date we're counting down to
-    var countDownDate = new Date(date).getTime();
-
+    const countDownDate = new Date(date).getTime();
+    
     // Update the count down every 1 second
     var x = setInterval(function() {
         // Get today's date and time
         var now = new Date().getTime();
-            
+        
         // Find the distance between now and the count down date
         var distance = countDownDate - now;
-            
+
         // Time calculations for days, hours, minutes and seconds
         var days = distance / (1000 * 60 * 60 * 24);
         days = (days > 0 ? Math.floor(days) : Math.ceil(days));
