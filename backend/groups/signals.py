ecfca843a9b8e3b683461ac831a027d20094cff8
bearthatcares <149488155+bearthatcares@users.noreply.github.com>
2024-04-26 09:18:52 -0400

    Add users to team/sig after approving request (#292)
    * Add users to team/sig after approving request
    
    * fix hooks

--- backend/groups/signals.py
+++ backend/groups/signals.py
@@ -14,11 +14,13 @@ logger = logging.getLogger(__name__)
     sender=SigRequest,
     dispatch_uid="sig_request_post_save",
 )
-def sig_request_post_save(sender, instance, created, **kwargs):
+def sig_request_post_save(sender, instance, **kwargs):
     logger.info("Sig request saved, updating user sigs")
     if instance.approved:
+        instance.sig.members.add(instance.user)
         instance.user.groups.add(instance.sig.group)
     elif instance.approved is False:
+        instance.sig.members.remove(instance.user)
         instance.user.groups.remove(instance.sig.group)
     else:
         pass
@@ -29,11 +31,13 @@ def sig_request_post_save(sender, instance, created, **kwargs):
     sender=TeamRequest,
     dispatch_uid="team_request_post_save",
 )
-def team_request_post_save(sender, instance, created, **kwargs):
+def team_request_post_save(sender, instance, **kwargs):
     logger.info("Team request saved, updating user teams")
     if instance.approved:
+        instance.team.members.add(instance.user)
         instance.user.groups.add(instance.team.group)
     elif instance.approved is False:
+        instance.team.members.remove(instance.user)
         instance.user.groups.remove(instance.team.group)
     else:
         pass
