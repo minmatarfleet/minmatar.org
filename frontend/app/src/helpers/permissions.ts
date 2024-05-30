d14fa44fa0d2aaa54f77940409b7629e8ee6347c
Beautiful Mim <beautifulmim.eve@gmail.com>
2024-05-30 12:16:41 -0400

    Return empty array for permissions exceptions

--- frontend/app/src/helpers/permissions.ts
+++ frontend/app/src/helpers/permissions.ts
@@ -3,8 +3,8 @@ import { get_user_by_name } from '@helpers/api.minmatar.org/authentication'
 export async function get_user_permissions(user_name:string) {
     try {
         const profile = await get_user_by_name(user_name)
-        return profile?.permissions
+        return profile?.permissions ?? []
     } catch (error) {
-        return null
+        return []
     }
 }
