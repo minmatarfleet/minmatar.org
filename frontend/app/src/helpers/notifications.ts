e3621c02afc0feb4052779a95c6e323c75141a8e
Beautiful Mim <156966699+beautifulmim@users.noreply.github.com>
2024-08-03 09:41:20 -0400

    Stripping html tags for push up notification body (#627)
    * Added meta descriptions on missing public pages
    
    * Bugfix friend page mobile cover
    
    * Rename friend cover for mobile
    
    * Updated submit application button color
    
    * Stripping html tags for push up notification body

--- frontend/app/src/helpers/notifications.ts
+++ frontend/app/src/helpers/notifications.ts
@@ -6,6 +6,9 @@ import { get_error_message } from '@helpers/string'
 const t = useTranslations('en')
 const translatePath = useTranslatedPath('en')
 
+import { marked } from 'marked';
+import * as cheerio from 'cheerio';
+
 import { get_player_icon } from '@helpers/eve_image_server';
 import { fetch_fleet_by_id, fetch_fleet_users } from '@helpers/fetching/fleets'
 
@@ -35,9 +38,11 @@ export async function send_active_fleet_notification(auth_token:string, fleet_id
 
     const fleet = await fetch_fleet_by_id(auth_token as string, fleet_id)
 
+    const $ = cheerio.load(await marked.parse(fleet.description))
+
     const payload = JSON.stringify({
         title: t('push_up_notification_title'),
-        body: fleet.description,
+        body: $('p:first').text(),
         icon: get_player_icon(fleet.fleet_commander_id),
         url: translatePath(`/fleets/upcoming/${fleet_id}`),
     });
