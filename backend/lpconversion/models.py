34c7a522d1dce4bbc75fe7a8aa0a37dc1d7f5543
Steve Leach <steve.leach@yahoo.com>
2024-08-26 16:33:46 +0100

    Add basic routes

--- backend/lpconversion/models.py
+++ backend/lpconversion/models.py
@@ -1,5 +1,45 @@
 lpconvert_status = "Unknown"
 
+lpconvert_data = ""
+
+lp_type_ids = [
+    41490,
+    32006,
+    32014,
+    21894,
+    21896,
+    21898,
+    21902,
+    21904,
+    21906,
+    21918,
+    21922,
+    21924,
+    31932,
+    31928,
+    31924,
+    31944,
+    41212,
+    41215,
+    41218,
+    33157,
+    29336,
+    17713,
+    72811,
+    31890,
+    31888,
+    31892,
+    31894,
+]
+
+lp_blueprint_ids = {
+    # Blueprint: Item
+    33158: 33157,
+    72923: 72811,
+    29339: 29336,
+    17714: 17713,
+}
+
 
 def set_status(status):
     global lpconvert_status
@@ -8,3 +48,12 @@ def set_status(status):
 
 def get_status():
     return lpconvert_status
+
+
+def set_item_data(data):
+    global lpconvert_data
+    lpconvert_data = data
+
+
+def get_item_data():
+    return lpconvert_data
--- backend/lpconversion/tasks.py
+++ backend/lpconversion/tasks.py
@@ -1,13 +1,22 @@
 import logging
+from datetime import datetime
 
 from app.celery import app
 
-from .models import set_status
+from .models import set_status, set_item_data, lp_type_ids
 
 logger = logging.getLogger(__name__)
 
 
 @app.task()
 def update_lpstore_items():
-    logger.info("Updating LP store item data")
-    set_status("Testing")
+    logger.info("Updating LP store item data...")
+    set_status("Updating...")
+    data = "type_id, name\n"
+
+    for type_id in lp_type_ids:
+        data += str(type_id) + ", 'Unknown'\n"
+
+    set_item_data(data)
+    set_status("Test " + datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    logger.info("Updated LP store item data.")
