89fed4bfdf70e736cd439b5d727f932a85071bb0
silvatek <85451322+silvatek@users.noreply.github.com>
2024-08-26 18:59:25 +0100

    Feature/lpconversion (#660)
    * Initial stub for LP conversion API
    
    * Fix global access
    
    * Don't use global for read
    
    * Add basic routes
    
    * Fix router path
    
    * Get quantities for a limited set of LP items

--- backend/lpconversion/router.py
+++ backend/lpconversion/router.py
@@ -1,10 +1,29 @@
 from ninja import Router
 
-from .models import get_status
+from django.http import HttpResponse
+
+from .models import get_status, get_item_data
+from .tasks import update_lpstore_items
 
 router = Router(tags=["conversion"])
 
 
-@router.get("/routes", response=str)
+@router.get("/lpitems/csv", response=str)
+def item_data(request):
+    response = HttpResponse(
+        content_type="text/csv",
+        headers={"Content-Disposition": 'attachment; filename="lp_items.csv"'},
+    )
+    response.write(get_item_data())
+    return response
+
+
+@router.get("/status", response=str)
 def conversion_status(request):
+    return get_status()
+
+
+@router.get("/refresh", response=str)
+def refresh_lpitem_data(request):
+    update_lpstore_items()
     return get_status()
--- backend/lpconversion/tasks.py
+++ backend/lpconversion/tasks.py
@@ -1,13 +1,78 @@
 import logging
+import requests
+import datetime
 
 from app.celery import app
 
-from .models import set_status
+from .models import set_status, set_item_data, lp_type_ids
+
+base_url = "https://esi.evetech.net/latest"
+region_forge = "10000002"
+corp_tlf = "1000182"
 
 logger = logging.getLogger(__name__)
 
 
+class TradedQuantities:
+    id: int
+    qty_1d: int
+    qty_7d: int
+    qty_30d: int
+
+
+def offset_date_string(offset_days):
+    return (
+        datetime.date.today() - datetime.timedelta(days=offset_days)
+    ).strftime("%Y-%m-%d")
+
+
+def get_item_name(type_id):
+    url = base_url + "/universe/types/" + str(type_id) + "/"
+    response = requests.get(url, timeout=5)
+    return response.json()["name"]
+
+
+def get_traded_quantities(type_id):
+    quantities = TradedQuantities()
+    quantities.id = type_id
+    quantities.qty_1d = 0
+    quantities.qty_7d = 0
+    quantities.qty_30d = 0
+
+    url = (
+        base_url
+        + "/markets/"
+        + region_forge
+        + "/history/?datasource=tranquility&type_id="
+        + str(type_id)
+    )
+    response = requests.get(url, timeout=5)
+
+    for item in response.json():
+        if item["date"] == offset_date_string(1):
+            quantities.qty_1d += item["volume"]
+        if item["date"] >= offset_date_string(7):
+            quantities.qty_7d += item["volume"]
+        if item["date"] >= offset_date_string(30):
+            quantities.qty_30d += item["volume"]
+
+    return quantities
+
+
 @app.task()
 def update_lpstore_items():
-    logger.info("Updating LP store item data")
-    set_status("Testing")
+    logger.info("Updating LP store item data...")
+    set_status("Updating...")
+    data = "type_id, item_name, qty_1d, qty_7d, qty_30d\n"
+
+    for type_id in lp_type_ids:
+        item_name = get_item_name(type_id)
+        quantities = get_traded_quantities(type_id)
+        data += (
+            str(type_id)
+            + f", {item_name}, {quantities.qty_1d}, {quantities.qty_7d}, {quantities.qty_30d}\n"
+        )
+
+    set_item_data(data)
+    set_status("Test " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    logger.info("Updated LP store item data.")
