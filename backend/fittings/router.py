4b31bd32091eb44dfea5d46ebd76dffbfb94805b
bearthatcares <bearthatcares.eve@gmail.com>
2024-08-30 10:41:17 -0400

    Add ideal fleet composition

--- backend/fittings/router.py
+++ backend/fittings/router.py
@@ -5,6 +5,7 @@ from ninja import Router
 from pydantic import BaseModel
 
 from .models import EveDoctrine, EveDoctrineFitting, EveFitting
+from app.errors import ErrorResponse
 
 doctrines_router = Router(tags=["Ships"])
 fittings_router = Router(tags=["Ships"])
@@ -37,6 +38,16 @@ class DoctrineResponse(BaseModel):
     secondary_fittings: List[FittingResponse]
     support_fittings: List[FittingResponse]
     sig_ids: List[int]
+
+
+class DoctrineCompositionItemResponse(BaseModel):
+    fitting: FittingResponse
+    ideal_ship_count: int
+
+
+class DoctrineCompositionResponse(BaseModel):
+    ideal_fleet_size: int
+    composition: List[DoctrineCompositionItemResponse]
 
 
 @doctrines_router.get("", response=List[DoctrineResponse])
@@ -122,6 +133,48 @@ def get_doctrine(request, doctrine_id: int):
         sig_ids=[sig.id for sig in doctrine.sigs.all()],
     )
     return doctrine_response
+
+
+@doctrines_router.get(
+    "/{doctrine_id}/composition",
+    response={
+        200: DoctrineCompositionResponse,
+        404: ErrorResponse,
+    },
+)
+def get_doctrine_composition(request, doctrine_id: int):
+    try:
+        doctrine = EveDoctrine.objects.get(id=doctrine_id)
+        fittings = EveDoctrineFitting.objects.filter(doctrine=doctrine)
+    except EveDoctrine.DoesNotExist:
+        return ErrorResponse(
+            status=404,
+            message="Doctrine details not found",
+        )
+
+    doctrine_items = []
+    for doctrine_fitting in fittings:
+        fitting = doctrine_fitting.fitting
+        composition_item = DoctrineCompositionItemResponse(
+            fitting=FittingResponse(
+                id=fitting.id,
+                name=fitting.name,
+                ship_id=fitting.ship_id,
+                description=fitting.description,
+                created_at=fitting.created_at,
+                updated_at=fitting.updated_at,
+                tags=[tag.name for tag in fitting.tags.all()],
+                eft_format=fitting.eft_format,
+                latest_version=fitting.latest_version,
+            ),
+            ideal_ship_count=doctrine_fitting.ideal_ship_count,
+        )
+        doctrine_items.append(composition_item)
+
+    return DoctrineCompositionResponse(
+        ideal_fleet_size=doctrine.ideal_fleet_size,
+        composition=doctrine_items,
+    )
 
 
 @fittings_router.get("", response=List[FittingResponse])
