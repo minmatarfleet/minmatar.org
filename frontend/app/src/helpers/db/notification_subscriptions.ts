ff84604d46e5a0132ee82e2ee48c337b9b727edd
Beautiful Mim <beautifulmim.eve@gmail.com>
2024-09-06 21:29:36 -0400

    Notification subscription checking on account page

--- frontend/app/src/helpers/db/notification_subscriptions.ts
+++ frontend/app/src/helpers/db/notification_subscriptions.ts
@@ -3,7 +3,8 @@ import { eq, and, or, sql } from 'drizzle-orm';
 import * as schema from '@/models/main/schema.ts';
 import type { NotificationSubscription } from '@dtypes/layout_components'
 
-export async function get_subscription_by_user(user_id:number) {
+export async function get_subscriptions_by_user(user_id:number) {
+    console.log('get_subscriptions_by_user')
     const q = await db.select({
         id: schema.fleet_push_subscriptions.id,
         subscription: schema.fleet_push_subscriptions.subscription,
@@ -12,16 +13,8 @@ export async function get_subscription_by_user(user_id:number) {
     .where(
         eq(schema.fleet_push_subscriptions.user_id, user_id)
     )
-    .limit(1)
 
-    if (q.length > 0) {
-        return {
-            id: q[0].id,
-            subscription: q[0].subscription,
-        } as NotificationSubscription
-    } else {
-        return null
-    }
+    return q as NotificationSubscription[]
 }
 
 export async function create_subscription(user_id:number, subscription:string) {
