f6a589d6fa3fd10fb1b458898f95142bb1481f2c
Beautiful Mim <156966699+beautifulmim@users.noreply.github.com>
2024-09-06 22:56:32 -0400

    Renamed get user subscriptions function (#698)
    * Notification subscription checking on account page
    
    * Removed fitting word from doctrines subtitles
    
    * Renamed get user subscriptions function

--- frontend/app/src/helpers/db/notification_subscriptions.ts
+++ frontend/app/src/helpers/db/notification_subscriptions.ts
@@ -3,7 +3,7 @@ import { eq, and, or, sql } from 'drizzle-orm';
 import * as schema from '@/models/main/schema.ts';
 import type { NotificationSubscription } from '@dtypes/layout_components'
 
-export async function get_subscriptions_by_user(user_id:number) {
+export async function get_user_subscriptions(user_id:number) {
     console.log('get_subscriptions_by_user')
     const q = await db.select({
         id: schema.fleet_push_subscriptions.id,
