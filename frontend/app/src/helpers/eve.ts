2aa3dfa6a276461fb0f85876057040f38f5ada5f
Beautiful Mim <156966699+beautifulmim@users.noreply.github.com>
2024-09-10 09:28:25 -0400

    Added fitting card for fittings page (#718)

--- frontend/app/src/helpers/eve.ts
+++ frontend/app/src/helpers/eve.ts
@@ -83,3 +83,53 @@ export const get_structure_id = (structure_type:string):number => {
 
     return STRUCTURE_TYPE_BY_ID[structure_type] ?? null
 }
+
+export const get_ship_type_icon = (ship_type:string):string => {
+    const SHIP_TYPE_ICONS = {
+        'Frigate': 'Isis_frigate.png',
+        'Electronic Attack Ship': 'Isis_frigate.png',
+        'Assault Frigate': 'Isis_frigate.png',
+        'Logistics Frigate': 'Isis_frigate.png',
+        'Covert Ops': 'Isis_frigate.png',
+        'Stealth Bomber': 'Isis_frigate.png',
+        'Interceptor': 'Isis_frigate.png',
+        'Destroyer': 'Isis_destroyer.png',
+        'Interdictor': 'Isis_destroyer.png',
+        'Command Destroyer': 'Isis_destroyer.png',
+        'Tactical Destroyer': 'Isis_destroyer.png',
+        'Cruiser': 'Isis_cruiser.png',
+        'Heavy Assault Cruiser': 'Isis_cruiser.png',
+        'Heavy Interdiction Cruiser': 'Isis_cruiser.png',
+        'Logistics': 'Isis_cruiser.png',
+        'Logistics Crusiers': 'Isis_cruiser.png',
+        'Strategic Cruiser': 'Isis_cruiser.png',
+        'Recon Ship': 'Isis_cruiser.png',
+        'Force Recon Ship': 'Isis_cruiser.png',
+        'Combat Recon Ship': 'Isis_cruiser.png',
+        'Battlecruiser': 'Isis_battlecruiser.png',
+        'Combat Battlecruiser': 'Isis_battlecruiser.png',
+        'Attack Battlecruiser': 'Isis_battlecruiser.png',
+        'Command Ship': 'Isis_battlecruiser.png',
+        'Battleship': 'Isis_battleship.png',
+        'Marauder': 'Isis_battleship.png',
+        'Black Ops': 'Isis_battleship.png',
+        'Capital': 'Isis_capital.png',
+        'Dreadnought': 'Isis_capital.png',
+        'Lancer Dreadnought': 'Isis_capital.png',
+        'Carrier': 'Isis_supercapital.png',
+        'Force Auxiliary': 'Isis_supercapital.png',
+        'Supercapital': 'Isis_supercapital.png',
+        'Supercarrier': 'Isis_supercarrier.png',
+        'Titan': 'Isis_titan.png',
+        'Miningfrigate': 'Isis_miningfrigate.png',
+        'Expedition Frigate': 'Isis_miningfrigate.png',
+        'Miningbarge': 'Isis_miningbarge.png',
+        'Industrial': 'Isis_industrial.png',
+        'Transport Ship': 'Isis_industrial.png',
+        'Freighter': 'Isis_industrial.png',
+        'Jump Freighters': 'Isis_freighter.png',
+        'Industrialcommand': 'Isis_industrialcommand.png',
+    }
+
+    return SHIP_TYPE_ICONS[ship_type] ?? null
+}
