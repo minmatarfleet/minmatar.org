fa0a07caeef9aea1495b44fd5b7f3c8dc7f7ede3
Steve Leach <steve.leach@yahoo.com>
2024-11-10 17:39:19 +0000

    Add support for zipped combat logs

--- backend/combatlog/router.py
+++ backend/combatlog/router.py
@@ -1,3 +1,7 @@
+import io
+import zipfile
+import logging
+
 from ninja import Router
 
 from .combatlog import (
@@ -14,7 +18,9 @@ from .combatlog import (
     LogAnalysis,
 )
 
-router = Router(tags=["combatlog"])
+router = Router(tags=["Combat Logs"])
+
+log = logging.getLogger(__name__)
 
 
 @router.post(
@@ -29,6 +35,13 @@ router = Router(tags=["combatlog"])
 )
 def analyze_logs(request):
     content = request.body.decode("utf-8")
+
+    analysis = analyze_parsed_log(content)
+
+    return analysis
+
+
+def analyze_parsed_log(content: str) -> LogAnalysis:
 
     events = parse(content)
 
@@ -52,3 +65,28 @@ def analyze_logs(request):
     update_combat_time(dmg_events, analysis)
 
     return analysis
+
+
+@router.post(
+    "/zipfile",
+    description="Process a zipped Eve combat log",
+    response={200: LogAnalysis},
+    openapi_extra={
+        "requestBody": {
+            "content": {
+                "application/zip": {
+                    "schema": {"type": "string", "format": "binary"}
+                }
+            }
+        }
+    },
+)
+def analyze_zipped_logs(request):
+    zipdata = io.BytesIO(request.body)
+
+    with zipfile.ZipFile(zipdata) as z:
+        zip_bytes = z.read(z.infolist()[0])
+
+    content = zip_bytes.decode("utf-8")
+
+    return analyze_parsed_log(content)
