01f46ef053afebff96f675b45fa02619b367a724
bearthatcares <bearthatcares.eve@gmail.com>
2024-12-13 10:14:58 -0500

    Add indexes to StandingFleetVoiceRecord for SQL querying

--- backend/standingfleet/models.py
+++ backend/standingfleet/models.py
@@ -6,3 +6,11 @@ class StandingFleetVoiceRecord(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     username = models.CharField(max_length=255)
     minutes = models.IntegerField()
+
+    class Meta:
+        indexes = [
+            # Index on 'created_on' for queries filtering by creation date
+            models.Index(fields=["created_on"], name="created_on_idx"),
+            # Index on 'username' for queries filtering by username
+            models.Index(fields=["username"], name="username_idx"),
+        ]
