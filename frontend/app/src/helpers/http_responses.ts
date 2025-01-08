95591935976d34e384b43fd9c6d18771ddf52bf9
Beautiful Mim <156966699+beautifulmim@users.noreply.github.com>
2025-01-08 10:20:44 -0500

    Update pages unauthorized responses (#993)
    * Updated unauth responses
    
    * Updated winter seasonal date
    
    * Added error 403 custom page

--- frontend/app/src/helpers/http_responses.ts
+++ frontend/app/src/helpers/http_responses.ts
@@ -5,24 +5,54 @@ export const HTTP_404_Not_Found = (body:BodyInit | null = null):Response => {
     });
 }
 
-/*import Page_403 from '@/pages/403.astro'
-import { experimental_AstroContainer as AstroContainer } from 'astro/container';*/
-
-export const HTTP_403_Forbidden = async () => {
-    /*const container = await AstroContainer.create();
-
+export const HTTP_403_Forbidden = () => {
     return new Response(
-        await container.renderToString(Page_403), {
+        `<!doctype html>
+            <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <title>403 Unauthorized</title>
+                    <meta name="description" content="Docking request denied!">
+                    <link rel="icon" type="image/svg+xml" href="/images/fleet-logo.png">
+
+                    <style>
+                        body {
+                            background-color: #121212;
+                        }
+                    </style>
+                    
+                    <script>
+                        async function fetchAndReplace(url) {
+                            try {
+                                // Fetch the new page content
+                                const response = await fetch(url);
+                    
+                                if (!response.ok) {
+                                    throw new Error(\`Error fetching the page: \${response.status} \${response.statusText}\`);
+                                }
+                    
+                                // Get the response as text
+                                const newHTML = await response.text();
+                    
+                                // Replace the entire page's HTML
+                                document.open(); // Clear the current document
+                                document.write(newHTML); // Write the new content
+                                document.close(); // Close the document to finalize changes
+                            } catch (error) {
+                                console.error("Error replacing page content:", error);
+                            }
+                        }
+                    
+                        fetchAndReplace("/403");
+                    </script>
+                </head>
+                <body></body>
+            </html>`, {
             status: 403,
             headers: { 'Content-type': 'text/html' },
             statusText: 'Forbidden',
         }
-    ) as Response;*/
-
-    return new Response(null, {
-        status: 403,
-        statusText: 'Forbidden'
-    });
+    ) as Response;
 }
 
 export const HTTP_200_Success = (body:BodyInit | null = null):Response => {
