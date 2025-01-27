334e5b4b280bd87a48ca004aab86abb8067bb9a2
Steve Leach <steve.leach@yahoo.com>
2025-01-27 23:21:21 +0000

    Use custom admin page for EveStructureTimer

--- backend/structures/admin.py
+++ backend/structures/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 from .models import EveStructure, EveStructureTimer
 
 admin.site.register(EveStructure)
-admin.site.register(EveStructureTimer)
+
+
+@admin.register(EveStructureTimer)
+class StructureTimerAdmin(admin.ModelAdmin):
+    list_display = ("name", "state")
+    search_fields = "name"
+    ordering = ("name",)
