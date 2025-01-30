8d5938e6896d4357399191e0f8f128fb9d3353d9
Steve Leach <steve.leach@yahoo.com>
2025-01-30 11:41:23 +0000

    Token scope investigation

--- backend/eveonline/tests/test_models.py
+++ backend/eveonline/tests/test_models.py
@@ -1,10 +1,20 @@
 import random
-
 import factory
+import logging
 from django.db.models import signals
+from django.contrib.auth.models import User
 
 from app.test import TestCase
-from eveonline.models import EveCharacter, EveCharacterSkill, EveSkillset
+from eveonline.models import (
+    EveCharacter,
+    EveCharacterSkill,
+    EveSkillset,
+    Token,
+    Scope,
+)
+
+
+logger = logging.getLogger("test.models")
 
 
 class EveSkillsetTestCase(TestCase):
@@ -138,3 +148,62 @@ class EveSkillsetTestCase(TestCase):
         self.assertFalse("Test Character 3" in result)
         self.assertFalse("Test Character 4" in result)
         self.assertEqual(len(result), 2)
+
+
+class TokenScopeTestCase(TestCase):
+    """
+    Tests methods for ESI token scopes.
+    """
+
+    def test_find_token_with_one_scope(self):
+        EveCharacter.objects.create(
+            character_id=123, character_name="Test Character 1"
+        )
+        user = User.objects.first()
+
+        scope1 = Scope.objects.create(name="scope1", help_text="")
+        scope2 = Scope.objects.create(name="scope2", help_text="")
+        scope3 = Scope.objects.create(name="scope3", help_text="")
+        Scope.objects.create(name="scope_unused", help_text="")
+
+        token1 = Token(user=user, character_id=123, character_name="Token1")
+        token1.save()
+        token1.scopes.add(scope1, scope2)
+
+        token2 = Token(user=user, character_id=123, character_name="Token2")
+        token2.save()
+        token2.scopes.add(scope2, scope3)
+
+        self.assertEqual(2, Token.objects.count())
+
+        token = Token.objects.filter(character_name="Token1").get()
+        self.assertEqual(2, token.scopes.count())
+
+        tokens = Token.objects.filter(
+            character_id=123, scopes__name__in=["scope1"]
+        )
+        self.assertEqual(1, tokens.count())
+
+        tokens = Token.objects.filter(
+            character_id=123, scopes__name__in=["scope1", "scope2"]
+        )
+        # Note that this returns 3 tokens, even though there are only 2 in the database
+        self.assertEqual(3, tokens.count())
+
+        tokens = Token.objects.filter(
+            character_id=123, scopes__name__in=["scope1", "scope2"]
+        ).distinct()
+        # Note that this returns 2 tokens, even though there are only 1 has those 2 scopes
+        self.assertEqual(2, tokens.count())
+
+        token = Token.get_token(123, ["scope1", "scope2"])
+        self.assertEqual("Token1", token.character_name)
+
+        token = Token.get_token(123, ["scope2", "scope3"])
+        self.assertEqual("Token2", token.character_name)
+
+        token = Token.get_token(123, ["scope1", "scope3"])
+        self.assertFalse(token)
+
+        token = Token.get_token(123, ["scope1", "scope_unused"])
+        self.assertFalse(token)
