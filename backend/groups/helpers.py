43ee253ef1e5867cf65bbcf1c4fc31f2bb799657
Steve Leach <steve.leach@yahoo.com>
2025-02-06 15:22:54 +0000

    Fix formatting

--- backend/groups/helpers.py
+++ backend/groups/helpers.py
@@ -1,14 +1,14 @@
 from django.contrib.auth.models import User
 from .models import Team
 
-PEOPLE_TEAM="People Team"
-TECH_TEAM="Technology Team"
+PEOPLE_TEAM = "People Team"
+TECH_TEAM = "Technology Team"
+
 
 def user_in_team(user: User, team_name: str) -> bool:
     team = Team.objects.filter(name=team_name).first()
-    
+
     if not team:
         raise ValueError(f"Unknown team: {team_name}")
-    
-    return team.members.contains(user)
 
+    return team.members.contains(user)
