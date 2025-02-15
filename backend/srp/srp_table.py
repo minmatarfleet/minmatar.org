5c2ac5c77d56ac55770cdb55b9d8bc547cc0668d
bearthatcares <bearthatcares.eve@gmail.com>
2025-02-15 11:45:46 -0500

    Update SRP table

--- backend/srp/srp_table.py
+++ backend/srp/srp_table.py
@@ -3,18 +3,19 @@ BILLION = 1000000000
 
 reimbursement_class_lookup = {
     "Force Auxiliary": 4 * BILLION,
-    "Logistics": 250 * MILLION,
+    "Logistics": 230050 * MILLION,
+    "Command Ship": 500 * MILLION,
     "Logistics Frigate": 50 * MILLION,
-    "Force Recon Ship": 250 * MILLION,
-    "Combat Recon Ship": 250 * MILLION,
-    "Heavy Interdiction Cruiser": 300 * MILLION,
-    "Interdictor": 75 * MILLION,
-    "Command Destroyer": 75 * MILLION,
-    "Interceptor": 25 * MILLION,
+    "Force Recon Ship": 275 * MILLION,
+    "Combat Recon Ship": 275 * MILLION,
+    "Heavy Interdiction Cruiser": 450 * MILLION,
+    "Interdictor": 100 * MILLION,
+    "Command Destroyer": 100 * MILLION,
+    "Interceptor": 50 * MILLION,
 }
 
 reimbursement_ship_lookup = {
     "Bhaalgorn": 750 * MILLION,
     "Vindicator": 750 * MILLION,
-    "Scorpion": 150 * MILLION,
+    "Scorpion": 30 * MILLION,
 }
