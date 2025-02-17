10f12f230869084ba44574dac5ee056a4603d1e4
silvatek <85451322+silvatek@users.noreply.github.com>
2025-02-17 00:32:45 +0000

    Revert "Added check for shared secret when deleting subscriptions (#1143)"
    This reverts commit 2dccf5a373c81ed964f6f594bf4f04f71c16afe0.

--- backend/subscriptions/router.py
+++ backend/subscriptions/router.py
@@ -5,7 +5,6 @@ from ninja import Router
 from pydantic import BaseModel
 
 from app.errors import ErrorResponse
-from app.settings import SHARED_SECRET
 from authentication import AuthBearer
 
 from .models import UserSubscription
@@ -65,11 +64,11 @@ def create_user_subscription(request, subscription: Subscription):
     auth=AuthBearer(),
     description="Deletes a user subscription record.",
 )
-def delete_user_subscription(request, sub_id: int, authcode: str = None):
+def delete_user_subscription(request, sub_id: int):
     sub = UserSubscription.objects.filter(id=sub_id).first()
     if not sub:
         return 404, ErrorResponse(detail=f"Subscription {sub_id} not found")
-    if (authcode != SHARED_SECRET) and (sub.user != request.user):
+    if sub.user != request.user:
         return 403, ErrorResponse(
             detail="Not permitted to delete this subscription"
         )
