88a75c7d16769e16f5c70a9e094e94c85e6a2768
Steve Leach <steve.leach@yahoo.com>
2025-02-20 17:30:33 +0000

    feature/set_shared_secret

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -27,6 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY", "testing")
+SHARED_SECRET = os.environ.get("SHARED_SECRET", "nothing")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
