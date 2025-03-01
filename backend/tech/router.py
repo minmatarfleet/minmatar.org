974cf1cc4f5e3da18c61cd2d2edf1a786a847dce
silvatek <85451322+silvatek@users.noreply.github.com>
2025-03-01 20:32:21 +0000

    Feature/esi client part 2 (#1163)
    * Next step of EsiClient implementation
    
    * Added non-token ESI request testing
    
    * Move get results into try block
    
    * Extract common operation_results code

--- backend/tech/router.py
+++ backend/tech/router.py
@@ -6,6 +6,7 @@ from django.conf import settings
 from app.errors import ErrorResponse
 from authentication import AuthBearer
 from groups.helpers import TECH_TEAM, user_in_team
+from eveonline.client import EsiClient
 
 router = Router(tags=["Tech"])
 logger = logging.getLogger(__name__)
@@ -43,3 +44,50 @@ def check_var(request, var_name: str) -> str:
         return "Set"
     else:
         return "Not set"
+
+
+@router.get(
+    "character/{int:character_id}",
+    summary="ESI debugging without token",
+    auth=AuthBearer(),
+    response={
+        200: str,
+        403: ErrorResponse,
+    },
+)
+def debug_character_esi(request, character_id: int):
+    """API endpoint for exploring the ESI client"""
+    if not (
+        request.user.is_superuser or user_in_team(request.user, TECH_TEAM)
+    ):
+        return 403, ErrorResponse(detail="Not authorised")
+
+    response = EsiClient(None).get_character_public_data(character_id)
+    if response.success():
+        char = response.results()
+        return char["name"]
+    else:
+        return f"Error {response.response_code}, {response.response}"
+
+
+@router.get(
+    "character/{int:character_id}/skills",
+    summary="ESI debugging with token",
+    auth=AuthBearer(),
+    response={
+        200: str,
+        403: ErrorResponse,
+    },
+)
+def debug_skills_esi(request, character_id: int):
+    if not (
+        request.user.is_superuser or user_in_team(request.user, TECH_TEAM)
+    ):
+        return 403, ErrorResponse(detail="Not authorised")
+
+    response = EsiClient(character_id).get_character_skills()
+    if response.success():
+        skills = response.results()
+        return str(len(skills))
+    else:
+        return f"Error {response.response_code}, {response.response}"
