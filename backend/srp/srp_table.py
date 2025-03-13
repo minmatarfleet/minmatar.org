46c1cf27d83323c92cd581fbcebee37e15b4e6c6
bearthatcares <bearthatcares.eve@gmail.com>
2025-03-13 10:16:54 -0400

    Update SRP values

--- backend/srp/srp_table.py
+++ backend/srp/srp_table.py
@@ -2,20 +2,30 @@ MILLION = 1000000
 BILLION = 1000000000
 
 reimbursement_class_lookup = {
-    "Force Auxiliary": 4 * BILLION,
+    "Force Auxiliary": 3 * BILLION,
+    "Carrier": 2 * BILLION,
+    "Dreadnought": 2 * BILLION,
     "Logistics": 300 * MILLION,
     "Command Ship": 500 * MILLION,
     "Logistics Frigate": 50 * MILLION,
     "Force Recon Ship": 275 * MILLION,
     "Combat Recon Ship": 275 * MILLION,
-    "Heavy Interdiction Cruiser": 450 * MILLION,
+    "Heavy Interdiction Cruiser": 400 * MILLION,
     "Interdictor": 100 * MILLION,
     "Command Destroyer": 100 * MILLION,
     "Interceptor": 50 * MILLION,
 }
 
 reimbursement_ship_lookup = {
-    "Bhaalgorn": 750 * MILLION,
-    "Vindicator": 750 * MILLION,
-    "Scorpion": 300 * MILLION,
+    "Revelation Navy Issue": 4000 * MILLION,
+    "Zirnitra": 4000 * MILLION,
+    "Phoenix Navy Issue": 4000 * MILLION,
+    "Bhaalgorn": 1250 * MILLION,
+    "Vindicator": 1250 * MILLION,
+    "Nestor": 1250 * MILLION,
+    "Barghest": 1250 * MILLION,
+    "Nightmare": 1250 * MILLION,
+    "Scorpion": 250 * MILLION,
+    "Zarmazd": 500 * MILLION,
+    "Loki": 500 * MILLION,
 }
