8fe73847a9e25da37e92220ef8e68e5c2882d51a
Steve Leach <steve.leach@yahoo.com>
2025-04-09 14:09:49 +0100

    Add cancelled fleets back to fleet list endpoint

--- backend/fleets/router.py
+++ backend/fleets/router.py
@@ -276,15 +276,12 @@ def get_v3_fleets(
         fleets = (
             EveFleet.objects.filter(evefleetinstance__end_time=None)
             .filter(start_time__gte=timezone.now() - timedelta(hours=1))
-            .exclude(status="cancelled")
             .order_by("-start_time")
         )
     elif fleet_filter == EveFleetFilter.UPCOMING:
-        fleets = (
-            EveFleet.objects.filter(start_time__gte=timezone.now())
-            .exclude(status="cancelled")
-            .order_by("-start_time")
-        )
+        fleets = EveFleet.objects.filter(
+            start_time__gte=timezone.now()
+        ).order_by("-start_time")
     else:
         # get fleets from past 30 days
         fleets = EveFleet.objects.filter(
