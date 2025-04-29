d3391b315b04ff0ad5bdc79a9c7e5936bc081c10
Steve Leach <steve.leach@yahoo.com>
2025-04-29 19:45:58 +0100

    Custom market contract admin page

--- backend/market/admin.py
+++ backend/market/admin.py
@@ -13,4 +13,24 @@ admin.site.register(EveMarketLocation)
 admin.site.register(EveMarketContractExpectation)
 admin.site.register(EveMarketItemExpectation)
 admin.site.register(EveMarketContractResponsibility)
-admin.site.register(EveMarketContract)
+
+
+@admin.register(EveMarketContract)
+class EveMarketContractAdmin(admin.ModelAdmin):
+    """
+    Custom admin to make working with contracts easier
+    """
+
+    list_display = (
+        "id",
+        "title",
+        "status",
+        "issuer_external_id",
+        "created_at",
+    )
+    search_fields = ("title", "issuer_external_id")
+    list_filter = (
+        "status",
+        "title",
+    )
+    date_hierarchy = "created_at"
