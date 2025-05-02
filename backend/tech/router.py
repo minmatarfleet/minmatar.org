ae73f56d460567d67eeabdb95dbd2ee9eb48a863
silvatek <85451322+silvatek@users.noreply.github.com>
2025-05-02 07:59:03 +0100

    Initial historic logs endpoint (#1270)
    * Initial historic logs endpoint
    
    * Add time window parameters

--- backend/tech/router.py
+++ backend/tech/router.py
@@ -1,5 +1,6 @@
 import logging
 import docker
+import datetime
 
 from ninja import Router
 from typing import List, Optional
@@ -123,3 +124,39 @@ def stream_logs(request, container_name: str):
     return StreamingHttpResponse(
         event_stream(), content_type="text/event-stream"
     )
+
+
+@router.get(
+    "/logs2/{container_name}",
+    description="Get historic logs for a specific container",
+    auth=AuthBearer(),
+    response={200: str, 403: ErrorResponse},
+)
+def get_logs(
+    request,
+    container_name: str,
+    start_delta_mins: int = 60,
+    duration_mins: int = 10,
+):
+    if not (
+        request.user.is_superuser or user_in_team(request.user, TECH_TEAM)
+    ):
+        return 403, "Not authorised"
+
+    client = docker.DockerClient(base_url="unix://var/run/docker.sock")
+    container = client.containers.get(container_name)
+
+    start_time = datetime.datetime.now() - datetime.timedelta(
+        minutes=start_delta_mins
+    )
+    end_time = start_time + datetime.timedelta(minutes=duration_mins)
+
+    content = container.logs(
+        stream=False,
+        stdout=True,
+        stderr=True,
+        since=start_time,
+        until=end_time,
+        timestamps=True,
+    )
+    return content.decode("utf-8")
