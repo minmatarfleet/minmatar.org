0228fd9ee05e4c94f903e9ecaf9ef6aa69f4bd71
Steve Leach <steve.leach@yahoo.com>
2025-05-08 21:35:11 +0100

    Expand Fleet admin page

--- backend/fleets/admin.py
+++ backend/fleets/admin.py
@@ -5,10 +5,27 @@ from .models import (
     EveFleetAudience,
     EveFleetAudienceWebhook,
     EveFleetLocation,
+    EveFleetInstance,
+    EveFleetInstanceMember,
 )
 
+
+class FleetAdmin(admin.ModelAdmin):
+    date_hierarchy = "start_time"
+    list_display = [
+        "start_time",
+        "status",
+        "description",
+        "created_by__username",
+    ]
+    list_filter = ["status"]
+    search_fields = ["description"]
+
+
 # Register your models here.
-admin.site.register(EveFleet)
+admin.site.register(EveFleet, FleetAdmin)
 admin.site.register(EveFleetLocation)
 admin.site.register(EveFleetAudience)
 admin.site.register(EveFleetAudienceWebhook)
+admin.site.register(EveFleetInstance)
+admin.site.register(EveFleetInstanceMember)
