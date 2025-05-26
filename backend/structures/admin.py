62bd67e0f8a6073a5090c9ed76cbfed599fd56a7
silvatek <85451322+silvatek@users.noreply.github.com>
2025-05-26 14:32:34 +0100

    Improve structure manager admin page (#1388)
    * Improve structure manager admin page
    
    * Linting
    
    * Remove broken line

--- backend/structures/admin.py
+++ backend/structures/admin.py
@@ -4,10 +4,29 @@ from .forms import EveStructureTimerForm
 from .models import EveStructure, EveStructureTimer, EveStructureManager
 
 admin.site.register(EveStructure)
-admin.site.register(EveStructureManager)
 
 
 @admin.register(EveStructureTimer)
 class StructureTimerAdmin(admin.ModelAdmin):
     form = EveStructureTimerForm
     list_display = ("name", "state")
+
+
+@admin.register(EveStructureManager)
+class StructureManagerAdmin(admin.ModelAdmin):
+    """Admin page for EveStructureManager"""
+
+    list_display = (
+        "id",
+        "corporation__name",
+        "character__character_name",
+        "poll_time",
+        "last_polled",
+    )
+    search_fields = ("corporation__name", "character__character_name")
+    list_filter = ("poll_time", "corporation__name")
+    list_display_links = (
+        "id",
+        "corporation__name",
+        "character__character_name",
+    )
