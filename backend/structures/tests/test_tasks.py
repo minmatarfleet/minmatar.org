fa1eeeb02443b9d9ec199558ce657253dbc28e12
silvatek <85451322+silvatek@users.noreply.github.com>
2025-05-26 01:11:15 +0100

    Initial structure notification task (#1382)

---
+++ backend/structures/tests/test_tasks.py
@@ -0,0 +1,50 @@
+import factory
+
+from django.db.models import signals
+
+from app.test import TestCase
+
+from eveonline.models import EveCorporation, EveCharacter
+
+from structures.tasks import (
+    setup_structure_managers,
+    structure_managers_for_minute,
+)
+from structures.models import EveStructureManager
+
+
+class StructureTimerTaskTests(TestCase):
+    """Task tests for structure timers"""
+
+    @factory.django.mute_signals(signals.pre_save, signals.post_save)
+    def test_setup_structure_managers(self):
+        self.assertEqual(0, EveStructureManager.objects.count())
+
+        corp = EveCorporation.objects.create(
+            corporation_id=1001,
+        )
+        chars = [
+            EveCharacter.objects.create(
+                character_id=2001,
+                character_name="Director 1",
+            ),
+            EveCharacter.objects.create(
+                character_id=2002,
+                character_name="Director 2",
+            ),
+            EveCharacter.objects.create(
+                character_id=2003,
+                character_name="CEO",
+            ),
+        ]
+
+        setup_structure_managers(corp, chars)
+
+        managers = EveStructureManager.objects.all().order_by("poll_time")
+        self.assertEqual(3, managers.count())
+        self.assertEqual(0, managers[0].poll_time)
+        self.assertEqual(3, managers[1].poll_time)
+        self.assertEqual(6, managers[2].poll_time)
+
+        minute_3 = structure_managers_for_minute(3)
+        self.assertEqual(1, minute_3.count())
