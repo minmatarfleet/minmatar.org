a21092f1446b8642f88240fb97122ed05946d73f
Steve Leach <steve.leach@yahoo.com>
2025-06-03 14:16:38 +0100

    Custom admin page for corp applications

--- backend/applications/admin.py
+++ backend/applications/admin.py
@@ -2,4 +2,20 @@ from django.contrib import admin
 
 from applications.models import EveCorporationApplication
 
-admin.site.register(EveCorporationApplication)
+
+class CorpApplicationAdmin(admin.ModelAdmin):
+    """Custom admin model for EveCorporationApplication entities"""
+
+    date_hierarchy = "created_at"
+    list_display = [
+        "id",
+        "created_at",
+        "user__username",
+        "corporation__name",
+        "status",
+    ]
+    list_filter = ["status"]
+    search_fields = ["user__username", "corporation__name"]
+
+
+admin.site.register(EveCorporationApplication, CorpApplicationAdmin)
