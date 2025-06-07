779a8f4ac1bb4945a0c3d56021801a9fe9018eb8
Steve Leach <steve.leach@yahoo.com>
2025-06-07 16:58:11 +0100

    Error IDs and discord exchange token error redirect

--- backend/app/errors.py
+++ backend/app/errors.py
@@ -1,5 +1,23 @@
 from pydantic import BaseModel
+from django.utils.crypto import get_random_string
 
 
 class ErrorResponse(BaseModel):
     detail: str
+    id: str | None = None
+
+    @classmethod
+    def new(cls, detail: str):
+        return cls(detail=detail, id=create_error_id())
+
+
+def create_error_id():
+    """
+    Make a random error ID in the form `xxxxx-xxxxx` to be used for debugging
+
+    Easily-mistaken characters are excluded from the ID.
+    """
+    allowed_chars = "ACDEFGHJKLMNPRTUVWXY345679"
+    r1 = get_random_string(length=5, allowed_chars=allowed_chars)
+    r2 = get_random_string(length=5, allowed_chars=allowed_chars)
+    return f"{r1}-{r2}"
