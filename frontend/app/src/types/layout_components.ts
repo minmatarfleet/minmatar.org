46dea221273bc42c3399982b9c0e60b5811da061
Beautiful Mim <156966699+beautifulmim@users.noreply.github.com>
2025-06-11 21:02:46 -0400

    Updated homepage stagings (#1484)

--- frontend/app/src/types/layout_components.ts
+++ frontend/app/src/types/layout_components.ts
@@ -647,7 +647,7 @@ export interface FittingGroup {
     modules:    CargoItem[];
 }
 
-export const stagings = ['auga', 'watermellon',  'nakah', 'vard', 'vard2', 'evati', 'amarr'] as const
+export const stagings = ['auga', 'watermellon',  'nakah', 'vard', 'evati', 'amarr'] as const
 export type StagingType = typeof stagings[number]
 
 import type { Doctrine, FleetTypes } from '@dtypes/api.minmatar.org'
