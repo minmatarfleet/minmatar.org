2a654f1013644f05f62f96db86effa2d537d6fba
Steve Leach <steve.leach@yahoo.com>
2025-06-13 13:26:55 +0100

    Clarify use of backend login for ESI SSO

--- backend/discord/views.py
+++ backend/discord/views.py
@@ -18,13 +18,28 @@ auth_url_discord = f"https://discord.com/api/oauth2/authorize?client_id={setting
 
 
 def discord_login(request: HttpRequest):  # pylint: disable=unused-argument
+    """
+    Backend login via Discord
+
+    See `users.router.login()` for frontend (website) login API.
+
+    Redirects the user to Discord; they will be redirected back which will
+    be handled by `discord_login_redirect`.
+
+    This backend login is used for...
+
+    1) Admin page access
+    2) Authentication during the ESI SSO process for getting/refreshing tokens
+    """
     if hasattr(settings, "FAKE_LOGIN_USER_ID"):
         return fake_login(request)
 
     # get next and store in session
-    logger.info("Adding redirect URL to session: %s", request.GET.get("next"))
+    logger.debug("Adding redirect URL to session: %s", request.GET.get("next"))
     request.session["next"] = request.GET.get("next")
-    logger.info("Redirecting to Discord for login %s", auth_url_discord)
+    logger.info(
+        "Redirecting to Discord for backend login %s", auth_url_discord
+    )
     return redirect(auth_url_discord)
 
 
@@ -48,19 +63,17 @@ def discord_login_redirect(request: HttpRequest):
 
     login(request, django_user)
 
-    logger.info(
-        "[DISCORD VIEW] :: admin logon successful for %s", django_user.username
-    )
-
     if request.session.get("next"):
         next_url = request.session["next"]
     else:
         next_url = "/admin"
 
-    logger.debug(
-        "[DISCORD VIEW] :: Redirecting to %s",
+    logger.info(
+        "[DISCORD VIEW] :: backend logon successful for %s, next = %s",
+        django_user.username,
         next_url,
     )
+
     return redirect(next_url)
 
 
