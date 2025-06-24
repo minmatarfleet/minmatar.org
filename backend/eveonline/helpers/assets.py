7c7e1a02dda91fbcaf98c98e2dda0d85b9374978
silvatek <85451322+silvatek@users.noreply.github.com>
2025-06-24 11:19:46 +0100

    Optimise character asset lookup (#1519)
    * Optimise character asset lookup
    
    * More asset optimisation

--- backend/eveonline/helpers/assets.py
+++ backend/eveonline/helpers/assets.py
@@ -27,6 +27,33 @@ class EveStructureResponse(pydantic.BaseModel):
     solar_system_id: int
 
 
+def non_ship_location(location_flag: str) -> bool:
+    if "FighterTube" in location_flag:
+        return True
+    if "Slot" in location_flag:
+        return True
+    if "Hold" in location_flag:
+        return True
+    if "Bay" in location_flag:
+        return True
+    if location_flag in [
+        "BoosterBay",
+        "CorpseBay",
+        "DroneBay",
+        "FighterBay",
+        "HiddenModifiers",
+        "Implant",
+        "InfrastructureHangar",
+        "Locked",
+        "Skill",
+        "Unlocked",
+        "Wardrobe",
+    ]:
+        return True
+
+    return False
+
+
 def create_character_assets(character: EveCharacter):
     """Create assets for a character"""
     updated = 0
@@ -35,19 +62,22 @@ def create_character_assets(character: EveCharacter):
     logger.debug("Creating assets for character %s", character.character_id)
     deleted, _ = EveCharacterAsset.objects.filter(character=character).delete()
     logger.debug("Loading assets for character %s", character.character_id)
+
+    not_ship = "NOT A SHIP"
+
+    # Cache of type ID to type names mappings
+    type_names = {}
+
     esi = EsiClient(None)
     assets: List[EveAssetResponse] = json.loads(character.assets_json)
     for asset in assets:
         logger.debug("Processing asset %s", asset)
         asset = EveAssetResponse(**asset)
-        eve_type = esi.get_eve_type(asset.type_id, True)
-        if eve_type.eve_group is None:
+
+        if non_ship_location(asset.location_flag):
             continue
-        eve_group = esi.get_eve_group(eve_type.eve_group.id, True)
-        eve_category = eve_group.eve_category
-        if eve_category.name != "Ship":
-            continue
-        logger.debug("Found asset %s", eve_type.name)
+
+        # Check location type first as it can rule out a lot of items quickly
         location = None
         if asset.location_type == "station":
             location = esi.get_station(asset.location_id)
@@ -59,9 +89,36 @@ def create_character_assets(character: EveCharacter):
         else:
             continue
 
+        if non_ship_location(asset.location_flag):
+            continue
+
+        if asset.is_blueprint_copy:
+            type_names[asset.type_id] = not_ship
+            continue
+
+        if asset.type_id in type_names:
+            # Already looked up this type
+            type_name = type_names[asset.type_id]
+            if type_name == not_ship:
+                continue
+        else:
+            # Lookup and cache name for type_id
+            eve_type = esi.get_eve_type(asset.type_id, True)
+            if eve_type.eve_group is None:
+                type_names[asset.type_id] = not_ship
+                continue
+            eve_group = esi.get_eve_group(eve_type.eve_group.id, True)
+            eve_category = eve_group.eve_category
+            if eve_category.name != "Ship":
+                type_names[asset.type_id] = not_ship
+                continue
+            type_name = eve_type.name
+            logger.debug("Found asset %s", type_name)
+            type_names[asset.type_id] = type_name
+
         EveCharacterAsset.objects.create(
             type_id=eve_type.id,
-            type_name=eve_type.name,
+            type_name=type_name,
             location_id=asset.location_id,
             location_name=location.name,
             character=character,
