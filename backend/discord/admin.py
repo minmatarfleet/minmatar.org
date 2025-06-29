ab4fd964c28ecc5dfeaac518f8ebfe82ec175d9a
silvatek <85451322+silvatek@users.noreply.github.com>
2025-06-29 13:38:21 +0100

    Admin page for discord roles (#1527)

--- backend/discord/admin.py
+++ backend/discord/admin.py
@@ -2,14 +2,19 @@ from django.contrib import admin
 
 from eveonline.helpers.characters import user_primary_character
 
-from .models import DiscordUser
+from .models import DiscordUser, DiscordRole
 
 
-# Register your models here.
 @admin.register(DiscordUser)
 class DiscordUserAdmin(admin.ModelAdmin):
-    list_display = ("discord_tag", "nickname", "primary_eve_character")
-    search_fields = ("discord_tag", "nickname")
+    list_display = ("discord_tag", "nickname", "primary_eve_character", "id")
+    search_fields = ("discord_tag", "nickname", "id")
 
     def primary_eve_character(self, obj):
         return user_primary_character(obj.user)
+
+
+@admin.register(DiscordRole)
+class DiscordRoleAdmin(admin.ModelAdmin):
+    list_display = ("role_id", "name")
+    search_fields = ("role_id", "name")
