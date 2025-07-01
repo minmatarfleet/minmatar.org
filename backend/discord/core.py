f2076f8ccd01f4a31f8be5144b35e19498a39c50
bearthatcares <149488155+bearthatcares@users.noreply.github.com>
2025-07-01 14:29:18 -0400

    Remove special modifiers to discord nicknames
    As we've grown in size, it's a bit harder to follow people with all the weird modifiers.

--- backend/discord/core.py
+++ backend/discord/core.py
@@ -6,13 +6,4 @@ def make_nickname(character, discord_user):
 
     nickname = f"[{corporation.ticker}] {character.character_name}"
 
-    if discord_user.is_down_under:
-        nickname = upsidedown.transform(nickname)
-
-    if discord_user.dress_wearer:
-        nickname = f"[👗] {character.character_name}"
-
-    if corporation.ticker == "-DRY-":
-        nickname = f"[ω] {character.character_name}"
-
     return nickname
