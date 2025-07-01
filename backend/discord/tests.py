a5f9483416b7c06de82dc23a8fa53483be6a32f6
bearthatcares <149488155+bearthatcares@users.noreply.github.com>
2025-07-01 14:36:34 -0400

    Update tests.py

--- backend/discord/tests.py
+++ backend/discord/tests.py
@@ -30,24 +30,6 @@ class DiscordSimpleTests(SimpleTestCase):
         discord = Mock(is_down_under=False, dress_wearer=False)
         self.assertEqual("[ABC] Bob", make_nickname(character, discord))
 
-    def test_down_under_nickname(self):
-        character = Mock(character_name="Bob", corporation=Mock(ticker="ABC"))
-        discord = Mock(is_down_under=True, dress_wearer=False)
-        self.assertEqual("qoᗺ [ƆᗺⱯ]", make_nickname(character, discord))
-
-    def test_dress_wearer(self):
-        character = Mock(character_name="Bob", corporation=Mock(ticker="ABC"))
-        discord = Mock(is_down_under=False, dress_wearer=True)
-        self.assertEqual("[👗] Bob", make_nickname(character, discord))
-
-    def test_dry_corp(self):
-        character = Mock(
-            character_name="Scott", corporation=Mock(ticker="-DRY-")
-        )
-        discord = Mock(is_down_under=False, dress_wearer=True)
-        self.assertEqual("[ω] Scott", make_nickname(character, discord))
-
-
 class DiscordSignalTests(TestCase):
     """
     Django tests for Discord signal functionality.
