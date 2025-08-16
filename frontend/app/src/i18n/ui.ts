e8389620e8ae6b5931692d7d0c700d3d7100f3f8
Beautiful Mim <156966699+beautifulmim@users.noreply.github.com>
2025-08-16 10:13:08 -0400

    Improved AAR link UI (#1581)

--- frontend/app/src/i18n/ui.ts
+++ frontend/app/src/i18n/ui.ts
@@ -1467,9 +1467,11 @@ export const ui = {
         'add_aar_link': 'Add AAR link',
         'edit_aar_link': 'Edit AAR link',
         'aar_link_dialog_title': 'AAR link',
+        'aar_link': 'AAR link',
         'aar_link_dialog_text': 'Add the link to the After Action Report (AAR)',
         'update_fleet_aar_link_error': 'An error occurred while updating the fleet AAR link.',
         'invalid_aar_link': 'AAR link is not valid.',
         'discord_aar_link': 'Discord AAR Link',
+        'aar_title': 'Read AAR in Discord',
     },
 } as const;
