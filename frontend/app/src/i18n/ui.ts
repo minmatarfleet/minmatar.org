64a4a6d32f43194c5610a59aba8f7296ce547677
Beautiful Mim <156966699+beautifulmim@users.noreply.github.com>
2025-10-16 08:56:29 -0400

    Added Yacht references (#1635)

--- frontend/app/src/i18n/ui.ts
+++ frontend/app/src/i18n/ui.ts
@@ -820,7 +820,7 @@ export const ui = {
         'visit': 'Visit',
         'destroyer_guide_title': 'The Navy Destroyer Metagame',
         'destroyer_guide_description': 'In this guide you will find all the common archetypes for the navy destroyers with several fits. Written by ',
-        'reddit_article': 'Read Reddit article',
+        'reddit_article': 'Read Reddit post',
         'guide_download': 'Open guide',
         'show_credentials': 'Show credentials',
         'username': 'Username',
