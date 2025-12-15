7e4879f0f2e344b8d5e8daf56322bd311b513b9d
Beautiful Mim <156966699+beautifulmim@users.noreply.github.com>
2025-12-15 18:13:34 -0500

    Removed Auga fort from stagings (#1665)

--- frontend/app/src/types/layout_components.ts
+++ frontend/app/src/types/layout_components.ts
@@ -652,7 +652,7 @@ export interface FittingGroup {
     modules:    CargoItem[];
 }
 
-export const stagings = ['auga', 'watermellon', 'slyce', 'amamake', 'lxq2', 'evati', 'amarr'] as const
+export const stagings = ['watermellon', 'slyce', 'amamake', 'lxq2', 'amarr'] as const
 export type StagingType = typeof stagings[number]
 
 import type { Doctrine, FleetTypes } from '@dtypes/api.minmatar.org'
