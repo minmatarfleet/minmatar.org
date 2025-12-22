155c75e49ac0ed143ff9ba8c88a3e4c88350d180
Beautiful Mim <156966699+beautifulmim@users.noreply.github.com>
2025-12-22 17:31:44 -0500

    Using tailwind prose for text content sections (#1682)

--- frontend/app/tailwind.config.js
+++ frontend/app/tailwind.config.js
@@ -1,10 +1,78 @@
 /** @type {import('tailwindcss').Config} */
 import animations from '@midudev/tailwind-animations'
+import typography from '@tailwindcss/typography'
 
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            color: 'var(--fleet-yellow)',
+            a: {
+              color: 'var(--highlight)',
+            },
+            h1: {
+              fontWeight: '500',
+              color: 'var(--highlight)',
+            },
+            h2: {
+              fontWeight: '500',
+              color: 'var(--highlight)',
+            },
+            h3: {
+              fontWeight: '500',
+              color: 'var(--highlight)',
+            },
+            h4: {
+              fontWeight: '600',
+              color: 'var(--highlight)',
+            },
+            h5: {
+              fontWeight: '600',
+              color: 'var(--highlight)',
+            },
+            h6: {
+              fontWeight: '600',
+              color: 'var(--highlight)',
+            },
+            ul: {
+              listStyleType: 'square',
+            },
+            strong: {
+              fontWeight: '600',
+              color: 'inherit',
+            },
+            b: {
+              fontWeight: '600',
+              color: 'inherit',
+            },
+            pre: {
+              border: 'solid 1px var(--border-color)',
+              backgroundColor: 'var(--background)',
+              borderRadius: '0',
+            },
+            code: {
+              fontWeight: 'inherit',
+              color: 'var(--highlight)',
+              border: 'solid 1px var(--border-color)',
+              borderRadius: '0',
+            },
+            blockquote: {
+              color: 'var(--highlight)',
+              borderInlineStartStyle: 'solid',
+              borderInlineStartColor: 'var(--highlight)',
+              paddingTop: '1px',
+              paddingBottom: '1px',
+            },
+            th: {
+              color: 'var(--highlight)',
+            }
+          },
+        },
+      },
+    },
   },
-  plugins: [ animations ],
+  plugins: [ animations, typography ],
 }
