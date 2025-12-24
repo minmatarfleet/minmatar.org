5c655a788ef2e5464c137ad581389db50b4d90cd
Beautiful Mim <156966699+beautifulmim@users.noreply.github.com>
2025-12-23 21:59:41 -0500

    Removed 8KE-YS staging (#1685)

--- frontend/app/src/types/layout_components.ts
+++ frontend/app/src/types/layout_components.ts
@@ -653,7 +653,7 @@ export interface FittingGroup {
     modules:    CargoItem[];
 }
 
-export const stagings = ['watermellon', 'slyce', 'amamake', 'lxq2', 'amarr'] as const
+export const stagings = ['watermellon', 'amamake', 'lxq2', 'amarr'] as const
 export type StagingType = typeof stagings[number]
 
 import type { Doctrine, FleetTypes } from '@dtypes/api.minmatar.org'
