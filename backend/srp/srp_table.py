96253e6ce4c0e968e80a841b157e001125127ea2
bearthatcares <149488155+bearthatcares@users.noreply.github.com>
2025-12-28 18:26:29 -0500

    Initialize empty reimbursement lookup dictionaries
    Initialized empty dictionaries for reimbursement lookups.

--- backend/srp/srp_table.py
+++ backend/srp/srp_table.py
@@ -2,9 +2,7 @@ MILLION = 1000000
 BILLION = 1000000000
 
 # Use table in database now
-reimbursement_class_lookup = {
-}
+reimbursement_class_lookup = {}
 
 # Use table in database now
-reimbursement_ship_lookup = {
-}
+reimbursement_ship_lookup = {}
