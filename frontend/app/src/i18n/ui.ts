a02cbc28e0449fc1612647b3f99490b2556422be
Beautiful Mim <beautifulmim.eve@gmail.com>
2026-01-28 18:03:33 -0500

    Minimal improvements to fitting page

--- frontend/app/src/i18n/ui.ts
+++ frontend/app/src/i18n/ui.ts
@@ -1598,5 +1598,7 @@ export const ui = {
         'all_doctrines': 'All doctrines',
         'no_market_locations': 'There are currently no market locations.',
         'all_locations': 'All locations',
+        'models_credits': '3D ship models credits to [EstamelGG/EVE_Model_Gallery](https://github.com/EstamelGG/EVE_Model_Gallery) ❤️',
+        'back_to_fittings': 'Back to fittings',
     },
 } as const;
