c0c1be73b6a3cf626246d9498c936295cf27e2ec
Beautiful Mim <beautifulmim.eve@gmail.com>
2026-03-04 10:58:55 -0500

    Added ICONS_BUG_PERCENTAGE and refactor to the feature

--- frontend/app/src/helpers/bug_icons.ts
+++ frontend/app/src/helpers/bug_icons.ts
@@ -1,7 +1,16 @@
-export const icons = [
+const icons = [
     '/images/bug_icons/kekw.png'
 ]
 
-export const get_random_icon = () => {
+const get_random_icon = () => {
     return icons[Math.floor(Math.random() * icons.length)];
+}
+
+import { icons_bug, icons_bug_percentage } from '@helpers/env';
+
+const FEATURE_ACTIVE = icons_bug()
+const BUG_PERCENTAGE = icons_bug_percentage()
+
+export const get_bug_icon = (icon:string) => {
+    return FEATURE_ACTIVE() && Math.random() <= BUG_PERCENTAGE ? get_random_icon() : icon
 }
