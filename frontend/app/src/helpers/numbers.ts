addfb47dd2e604e07c8178692077852a84a33084
Beautiful Mim <156966699+beautifulmim@users.noreply.github.com>
2026-05-15 11:14:11 -0400

    Simplified version for numbers on notification badges (#2270)

--- frontend/app/src/helpers/numbers.ts
+++ frontend/app/src/helpers/numbers.ts
@@ -25,3 +25,29 @@ export const number_thousand_separator = (val: number, locale:string = 'en-US') 
         maximumFractionDigits: 0,
     })
 }
+
+/**
+ * Converts a number to a string with a suffix (k, M, B, T).
+ * @param num The number to format
+ * @param digits The number of decimal places to keep
+ */ 
+export function format_number(num: number, digits: number = 1): string {
+    const lookup = [
+        { value: 1, symbol: "" },
+        { value: 1e3, symbol: "k" },
+        { value: 1e6, symbol: "M" },
+        { value: 1e9, symbol: "B" },
+        { value: 1e12, symbol: "T" },
+        { value: 1e15, symbol: "P" },
+        { value: 1e18, symbol: "E" }
+    ];
+
+    const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+    const item = lookup.slice().reverse().find((item) => {
+        return num >= item.value;
+    });
+
+    return item 
+        ? (num / item.value).toFixed(digits).replace(rx, "$1") + item.symbol 
+        : "0";
+}
