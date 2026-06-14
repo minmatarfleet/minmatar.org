5fd8446eb544790cf3a10bb41b8f36eb3c3cea73
Beautiful Mim <beautifulmim.eve@gmail.com>
2026-06-14 11:23:34 -0400

    Removed old R6 staging

--- frontend/app/src/helpers/covers.ts
+++ frontend/app/src/helpers/covers.ts
@@ -1,23 +1,21 @@
 const AMAMAKE_LOCATION_ID = 1022167642188
-const R_6KYM_LOCATION_ID = 1051465618860
+const R_6KYM_2_LOCATION_ID = 1053229023468
 
 const lookup_location_name = (location_id:number) => {
     if (location_id === AMAMAKE_LOCATION_ID) return 'Amamake - 5 times nearly AT winners'
-    if (location_id === R_6KYM_LOCATION_ID) return 'R-6KYM - DO NOT USE'
+    if (location_id === R_6KYM_2_LOCATION_ID) return 'R-6KYM - Casper Anchored It'
 
     return null
 }
 
 export const get_location_video_cover = (location: string | number) => {
     const LOCATION_VIDEO_COVERS = {
-        'R-6KYM - DO NOT USE': '/videos/r6-loop.webm',
         'R-6KYM - Casper Anchored It': '/videos/r6-2-loop.webm',
         'Amamake - 5 times nearly AT winners': '/videos/amamake-loop.webm',
         'Jita': '/videos/jita-loop.webm',
         'Jita IV - Moon 4 - Caldari Navy Assembly Plant': '/videos/jita-loop.webm',
         'amarr': '/videos/emperor-loop.webm',
         'amamake': '/videos/amamake-loop.webm',
-        'r6': '/videos/r6-loop.webm',
         'r6_2': '/videos/r6-2-loop.webm',
     }
 
@@ -28,14 +26,12 @@ export const get_location_video_cover = (location: string | number) => {
 
 export const get_location_video_cover_thumb = (location: string | number) => {
     const LOCATION_VIDEO_COVERS_THUMBS = {
-        'R-6KYM - DO NOT USE': '/images/home-r6-cover.jpg',
         'R-6KYM - Casper Anchored It': '/images/home-r6-2-cover.jpg',
         'Amamake - 5 times nearly AT winners': '/images/home-amamake-cover.jpg',
         'Jita': '/images/home-jita-cover.jpg',
         'Jita IV - Moon 4 - Caldari Navy Assembly Plant': '/images/home-jita-cover.jpg',
         'amarr': '/images/home-amarr-cover.jpg',
         'amamake': '/images/home-amamake-cover.jpg',
-        'r6': '/images/home-r6-cover.jpg',
         'r6_2': '/images/home-r6-2-cover.jpg',
     }
 
@@ -46,14 +42,12 @@ export const get_location_video_cover_thumb = (location: string | number) => {
 
 export const get_location_cover = (location: string | number) => {
     const LOCATION_COVERS = {
-        'R-6KYM - DO NOT USE': '/images/home-r6-cover-eve.jpg',
         'R-6KYM - Casper Anchored It': '/images/home-r6-2-cover-eve.jpg',
         'Amamake - 5 times nearly AT winners': '/images/home-amamake-cover-eve.jpg',
         'Jita': '/images/home-jita-cover-eve.jpg',
         'Jita IV - Moon 4 - Caldari Navy Assembly Plant': '/images/home-jita-cover-eve.jpg',
         'amarr': '/images/home-amarr-cover-eve.jpg',
         'amamake': '/images/home-amamake-cover-eve.jpg',
-        'r6': '/images/home-r6-cover-eve.jpg',
         'r6_2': '/images/home-r6-2-cover-eve.jpg',
     }
 
@@ -64,14 +58,12 @@ export const get_location_cover = (location: string | number) => {
 
 export const get_location_cover_990 = (location: string | number) => {
     const LOCATION_COVERS_990 = {
-        'R-6KYM - DO NOT USE': '/images/home-r6-cover-eve.jpg',
         'R-6KYM - Casper Anchored It': '/images/home-r6-2-cover-eve.jpg',
         'Amamake - 5 times nearly AT winners': '/images/home-amamake-cover-eve.jpg',
         'Jita': '/images/home-jita-cover-eve.jpg',
         'Jita IV - Moon 4 - Caldari Navy Assembly Plant': '/images/home-jita-cover-eve.jpg',
         'amarr': '/images/home-amarr-cover-eve.jpg',
         'amamake': '/images/home-amamake-cover-eve.jpg',
-        'r6': '/images/home-r6-cover-eve.jpg',
         'r6_2': '/images/home-r6-2-cover-eve.jpg',
     }
 
