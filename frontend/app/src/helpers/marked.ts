acfc8b3897eb172d4be9b46c7fdb4dc9e39967c1
Beautiful Mim <156966699+beautifulmim@users.noreply.github.com>
2026-07-18 15:43:28 -0400

    App form quick overhaul (#2447)

--- frontend/app/src/helpers/marked.ts
+++ frontend/app/src/helpers/marked.ts
@@ -4,42 +4,49 @@ import { useTranslations, useTranslatedPath } from '@i18n/utils';
 import { marked } from 'marked';
 import { get_app_url } from '@helpers/env'
 
-export const renderer = new marked.Renderer()
+export function getRenderer(open_in_new_tab: boolean = false) {
+	const renderer = new marked.Renderer()
+	const linkRenderer = renderer.link
 
-const linkRenderer = renderer.link
+	renderer.link = (href, title, text) => {
+		let replace = ''
 
-renderer.link = (href, title, text) => {
-	let replace = ''
+		if (href.startsWith('https://my.minmatar.org')) {
+			href = href.replace('https://my.minmatar.org', '')
+		}
 
-	if (href.startsWith('https://my.minmatar.org')) {
-		href = href.replace('https://my.minmatar.org', '')
+		const localLink = href.startsWith(`${get_app_url()}`) || href.startsWith('/')
+
+		if (!localLink) {
+			const lang = 'en'
+			const t = useTranslations(lang)
+			const translatePath = useTranslatedPath(lang)
+
+			const anchor = `target="_blank"
+				rel="nofollow noopener noreferrer"
+				x-data="{
+					hide_external_link_disclaimer: $persist(false),
+					show_external_link_disclaimer() {
+						show_alert_dialog({
+							title: '${t('leaving_website')}',
+							partial: '${translatePath('/partials/external_link_disclaimer_dialog/')}',
+						}).then(accepted => {
+							if (accepted) window.open('HREF', '_blank')
+						})
+					}
+				}"
+				x-on:click.prevent="hide_external_link_disclaimer ? window.open('HREF', '_blank') : show_external_link_disclaimer()"`
+
+			replace = anchor.replaceAll('HREF', href)
+		} else if (open_in_new_tab) {
+			replace = 'target="_blank" rel="noopener noreferrer"'
+		}
+
+		const html = linkRenderer.call(renderer, href, title, text)
+		return localLink && !open_in_new_tab ? html : html.replace(/^<a /, `<a ${replace} `)
 	}
 
-	const localLink = href.startsWith(`${get_app_url()}`) || href.startsWith('/')
-
-	if (!localLink) {
-		const lang = 'en'
-		const t = useTranslations(lang)
-		const translatePath = useTranslatedPath(lang)
-
-		const anchor = `target="_blank"
-			rel="nofollow noopener noreferrer"
-			x-data="{
-				hide_external_link_disclaimer: $persist(false),
-				show_external_link_disclaimer() {
-					show_alert_dialog({
-						title: '${t('leaving_website')}',
-						partial: '${translatePath('/partials/external_link_disclaimer_dialog/')}',
-					}).then(accepted => {
-						if (accepted) window.open('HREF', '_blank')
-					})
-				}
-			}"
-			x-on:click.prevent="hide_external_link_disclaimer ? window.open('HREF', '_blank') : show_external_link_disclaimer()"`
-
-		replace = anchor.replaceAll('HREF', href)
-	}
-
-	const html = linkRenderer.call(renderer, href, title, text)
-	return localLink ? html : html.replace(/^<a /, `<a ${replace} `)
+	return renderer
 }
+
+export const renderer = getRenderer()
